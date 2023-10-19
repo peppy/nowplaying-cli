ebab629ccd79a60e7523d83406f0e4dd48d4c9cb
Tim Oliver <git@tim.dev>
2023-10-19 12:54:49 +0900

    Wrap main run loop in an autoreleasepool

--- src/nowplaying.mm
+++ src/nowplaying.mm
@@ -29,67 +29,69 @@ NSObject* getInfoForKey(NSDictionary* information, NSString* key) {
 int main(int argc, char** argv) {
     while(true)
     {
-        CFURLRef ref = (__bridge CFURLRef) [NSURL fileURLWithPath:@"/System/Library/PrivateFrameworks/MediaRemote.framework"];
-        CFBundleRef bundle = CFBundleCreate(kCFAllocatorDefault, ref);
-
-        MRMediaRemoteGetNowPlayingInfoFunction MRMediaRemoteGetNowPlayingInfo = (MRMediaRemoteGetNowPlayingInfoFunction) CFBundleGetFunctionPointerForName(bundle, CFSTR("MRMediaRemoteGetNowPlayingInfo"));
-        MRMediaRemoteGetNowPlayingInfo(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(NSDictionary* information) {
-            NSString* title = (NSString*)getInfoForKey(information, [NSString stringWithFormat:@"title"]);
-            NSString* artist = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"artist"]);
-            NSString* album = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"album"]);
-            NSString* userInfo = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"userInfo"]);
-            NSString* artworkIdentifier = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"artworkIdentifier"]);
-            artworkIdentifier = [NSString stringWithFormat:@"%lu.jpg", artworkIdentifier.hash];
-            NSData* artworkData = (NSData*) getInfoForKey(information, [NSString stringWithFormat:@"artworkData"]);
-            NSString* artworkDataHeight = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"artworkDataHeight"]);
-            NSString* duration = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"duration"]);
-            NSString* elapsedTime = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"elapsedTime"]);
-
-            NSString* podArtURL = @"";
-            NSError *error = NULL;
-
-            // this isn't really required.
-            if (userInfo != nil)
-            {
-                NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"https:[^\"]*" options:NSRegularExpressionCaseInsensitive error:&error];
-                NSTextCheckingResult *match = [regex firstMatchInString:userInfo options:0 range:NSMakeRange(0, [userInfo length])];
-
-                if (match){
-                    podArtURL = [userInfo substringWithRange:[match range]];
-                    podArtURL = [podArtURL stringByReplacingOccurrencesOfString:@"{w}" withString:@"256"];
-                    podArtURL = [podArtURL stringByReplacingOccurrencesOfString:@"{h}" withString:@"256"];
-                    podArtURL = [podArtURL stringByReplacingOccurrencesOfString:@"{f}" withString:@"jpg"];
-                    podArtURL = [podArtURL stringByReplacingOccurrencesOfString:@"{c}" withString:@""];
-                } 
-            }
-
-            if (artworkDataHeight != nil && artworkDataHeight.length > 0)
-            {
-                NSString* filePath = [NSString stringWithFormat:@"../cache/%@", artworkIdentifier];
-                NSFileManager *fileManager = [NSFileManager defaultManager];
-                if(![fileManager fileExistsAtPath:filePath]) {
-                    if (artworkData != nil) {
-                        [artworkData writeToFile:filePath options:NSDataWritingAtomic error:&error];
-                    } else {
-                        // having a non-null height means we have pending artwork data
-                        // it may take a bit for it to arrive.
-                        // don't update output until it does.
-                        sleep(1);
-                        return;
+        @autoreleasepool {
+            CFURLRef ref = (__bridge CFURLRef) [NSURL fileURLWithPath:@"/System/Library/PrivateFrameworks/MediaRemote.framework"];
+            CFBundleRef bundle = CFBundleCreate(kCFAllocatorDefault, ref);
+    
+            MRMediaRemoteGetNowPlayingInfoFunction MRMediaRemoteGetNowPlayingInfo = (MRMediaRemoteGetNowPlayingInfoFunction) CFBundleGetFunctionPointerForName(bundle, CFSTR("MRMediaRemoteGetNowPlayingInfo"));
+            MRMediaRemoteGetNowPlayingInfo(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(NSDictionary* information) {
+                NSString* title = (NSString*)getInfoForKey(information, [NSString stringWithFormat:@"title"]);
+                NSString* artist = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"artist"]);
+                NSString* album = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"album"]);
+                NSString* userInfo = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"userInfo"]);
+                NSString* artworkIdentifier = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"artworkIdentifier"]);
+                artworkIdentifier = [NSString stringWithFormat:@"%lu.jpg", artworkIdentifier.hash];
+                NSData* artworkData = (NSData*) getInfoForKey(information, [NSString stringWithFormat:@"artworkData"]);
+                NSString* artworkDataHeight = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"artworkDataHeight"]);
+                NSString* duration = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"duration"]);
+                NSString* elapsedTime = (NSString*) getInfoForKey(information, [NSString stringWithFormat:@"elapsedTime"]);
+    
+                NSString* podArtURL = @"";
+                NSError *error = NULL;
+    
+                // this isn't really required.
+                if (userInfo != nil)
+                {
+                    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"https:[^\"]*" options:NSRegularExpressionCaseInsensitive error:&error];
+                    NSTextCheckingResult *match = [regex firstMatchInString:userInfo options:0 range:NSMakeRange(0, [userInfo length])];
+    
+                    if (match){
+                        podArtURL = [userInfo substringWithRange:[match range]];
+                        podArtURL = [podArtURL stringByReplacingOccurrencesOfString:@"{w}" withString:@"256"];
+                        podArtURL = [podArtURL stringByReplacingOccurrencesOfString:@"{h}" withString:@"256"];
+                        podArtURL = [podArtURL stringByReplacingOccurrencesOfString:@"{f}" withString:@"jpg"];
+                        podArtURL = [podArtURL stringByReplacingOccurrencesOfString:@"{c}" withString:@""];
+                    } 
+                }
+    
+                if (artworkDataHeight != nil && artworkDataHeight.length > 0)
+                {
+                    NSString* filePath = [NSString stringWithFormat:@"../cache/%@", artworkIdentifier];
+                    NSFileManager *fileManager = [NSFileManager defaultManager];
+                    if(![fileManager fileExistsAtPath:filePath]) {
+                        if (artworkData != nil) {
+                            [artworkData writeToFile:filePath options:NSDataWritingAtomic error:&error];
+                        } else {
+                            // having a non-null height means we have pending artwork data
+                            // it may take a bit for it to arrive.
+                            // don't update output until it does.
+                            sleep(1);
+                            return;
+                        }
                     }
                 }
-            }
-
-            NSArray *allStrings = 
-                artist != nil && artist.length > 0 
-                ? @[@"true", @"Music", title, artist, album, podArtURL, artworkIdentifier, duration, elapsedTime]
-                : @[@"false"];
-
-            NSString *joined = [allStrings componentsJoinedByString:@"\n"];
-            [joined writeToFile:@"track" atomically:YES encoding:NSUTF8StringEncoding error:&error];
-        });
-
-        sleep(1);
+    
+                NSArray *allStrings = 
+                    artist != nil && artist.length > 0 
+                    ? @[@"true", @"Music", title, artist, album, podArtURL, artworkIdentifier, duration, elapsedTime]
+                    : @[@"false"];
+    
+                NSString *joined = [allStrings componentsJoinedByString:@"\n"];
+                [joined writeToFile:@"track" atomically:YES encoding:NSUTF8StringEncoding error:&error];
+            });
+    
+            sleep(1);
+        }
     }
 }
 
